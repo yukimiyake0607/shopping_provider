b2a96f6da9a32c5daf2bf8b070d15b6b0ed629cd
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-03 19:21:58 +0900

    add: ビルド時に表示されるBottomNavigationScreenを作成

--- lib/main.dart
+++ lib/main.dart
@@ -22,3 +22,78 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+class BottomNavigationScreen extends StatefulWidget {
+  const BottomNavigationScreen({super.key});
+
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  final List<Widget> _screensList = [
+    HomeScreen(),
+    ProductScreen(),
+    FavoriteScreen(),
+    CartScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Consumer<BottomNavModel>(
+        builder: (context, bottomNavModel, child) {
+          return _screensList[bottomNavModel.selectedPageNumber];
+        },
+      ),
+      bottomNavigationBar: Consumer<BottomNavModel>(
+        builder: (context, bottomNavModel, child) {
+          return BottomNavigationBar(
+            currentIndex: bottomNavModel.selectedPageNumber,
+            type: BottomNavigationBarType.fixed,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'ホーム',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: '探す',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_outline),
+                label: 'お気に入り',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'カート',
+              ),
+            ],
+            unselectedItemColor: Colors.black,
+            selectedItemColor: Colors.grey,
+            unselectedLabelStyle: const TextStyle(color: Colors.black),
+            selectedLabelStyle: const TextStyle(color: Colors.grey),
+            onTap: (value) {
+              bottomNavModel.updatedPageNumber(value);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class BottomNavModel with ChangeNotifier {
+  int _selectedPageNumber = 0;
+
+  //getter
+  int get selectedPageNumber => _selectedPageNumber;
+
+  void updatedPageNumber(int index) {
+    _selectedPageNumber = index;
+    notifyListeners();
+  }
+}
