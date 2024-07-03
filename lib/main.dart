c10e8ffd110456e2fdb512390c179c4cf7cead6c
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-03 19:20:04 +0900

    add: MainAppにProviderで状態管理するためChangeNotifierProviderを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_provider/screens/cart_screen.dart';
+import 'package:shopping_provider/screens/favorite_screen.dart';
+import 'package:shopping_provider/screens/home_screen.dart';
+import 'package:shopping_provider/screens/product_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +14,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => BottomNavModel(),
+      child: MaterialApp(
+        home: BottomNavigationScreen(),
       ),
     );
   }
