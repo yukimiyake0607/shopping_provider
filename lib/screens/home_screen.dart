43ff29871d15f635353b84ca206781ad6d7e93ae
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-05 13:01:05 +0900

    add: ItemContainerクラスを作成し、home_screenに追加
    refs ItemContainerは商品に必要な情報を追加し、ホーム画面に載せるためのもの

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,69 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Clothing Store'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('この夏おすすめの注目アイテム'),
+            Row(
+              children: [
+                ItemContainer(
+                    itemImageUrl:
+                        'https://image.salesnauts.com/acrmtsm/img/NcSjyHjc4ERt.jpg?width=768',
+                    itemName: 'プリントTシャツ',
+                    itemPrice: '14,300')
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemContainer extends StatelessWidget {
+  String itemImageUrl;
+  String itemName;
+  String itemPrice;
+
+  ItemContainer(
+      {required this.itemImageUrl,
+      required this.itemName,
+      required this.itemPrice});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Image.network(
+              itemImageUrl,
+            ),
+          ),
+          Text(itemName),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(child: Text('￥$itemPrice')),
+              Consumer<FavoriteModel>(
+                builder: (context, favoriteModel, child) {
+                  return GestureDetector(
+                    onTap: () {
+                      favoriteModel.getFavorite();
+                    },
+                    child: favoriteModel.isFavorite
+                        ? Icon(Icons.favorite)
+                        : Icon(Icons.favorite_outline),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
